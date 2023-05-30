dfa7d9eb3fb7c6f9e8fdf32a1774256b83ca426c
Fernando Reyes <fernandoreyes.dev@gmail.com>
2023-05-30 15:27:20 -0700

    added pyswip library for prolog
    Signed-off-by: Fernando Reyes <fernandoreyes.dev@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request
+from pyswip import Prolog
+
+# CODIGO PARA PROLOG
+prolog = Prolog()
+prolog.consult('./static/basePeliculas.pl')
+
+# SE NECESITAN REMPLAZAR PARAMETROS POR VALORES PERO ASI FUNCIONARIA
+# prolog.query("pelicula_por_titulo(Pelicula, Titulo)")
 
 app = Flask(__name__)
 
@@ -11,6 +19,17 @@ def results():
     genre = request.form['genre']
     year = request.form['year']
     director = request.form['director']
+
+    if year == "":
+        year = "null"
+
+    # AQUI SE HARAN LAS CONSULTAS DE PROLOG CON LOS VALORES RECIBIDOS (genre, year, director)
+    # SI LOS VALORES LLEVAN null COMO VALOR, IGNORARLOS Y RELIZAR CONSULTA SIN ELLOS
+
+
+
+
+
 
     return render_template("results.html", genre=genre, year=year, director=director)
 

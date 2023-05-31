bff2b0e8f005e70fe0845b89539d44b549c59170
John Doe <urielnol20@gmail.com>
2023-05-30 21:11:47 -0600

    Igualado a la version Main

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask, render_template, request
+from pyswip import Prolog
+
+# CODIGO PARA PROLOG
+prolog = Prolog()
+prolog.consult('./static/basePeliculas.pl')
+
+# SE NECESITAN REMPLAZAR PARAMETROS POR VALORES PERO ASI FUNCIONARIA
+# prolog.query("pelicula_por_titulo(Pelicula, Titulo)")
 
 app = Flask(__name__)
 
@@ -8,10 +16,22 @@ def index():
 
 @app.route("/results", methods=["POST","GET"])
 def results():
-    username = request.form['username']
-    password = request.form['password']
+    genre = request.form['genre']
+    year = request.form['year']
+    director = request.form['director']
 
-    return render_template("results.html", username=username, password=password)
+    if year == "":
+        year = "null"
+
+    # AQUI SE HARAN LAS CONSULTAS DE PROLOG CON LOS VALORES RECIBIDOS (genre, year, director)
+    # SI LOS VALORES LLEVAN null COMO VALOR, IGNORARLOS Y RELIZAR CONSULTA SIN ELLOS
+
+
+
+
+
+
+    return render_template("results.html", genre=genre, year=year, director=director)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
